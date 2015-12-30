a392902e51ce40a588e3f7b3df247a94ed201c4a
venetucci <michelle.ann.harvey@gmail.com>
2015-12-29 22:48:17 -0800

    Create initial list view with name array

--- acquainted/ViewController.swift
+++ acquainted/ViewController.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var MainList: UITableView!
+    
+    let acquaintances = ["Mom", "Nick", "Aaron", "Anya", "Heather"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        MainList.delegate = self
+        MainList.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = acquaintances[indexPath.item]
+        
+        return cell
     }
 
 
